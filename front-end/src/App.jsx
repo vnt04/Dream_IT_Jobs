d47c1737b221de65743075ea272309832577e9b2
vnt04 <84673577+vnt04@users.noreply.github.com>
2024-04-08 15:09:23 +0700

    set up project

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold ">Hello, This is my new project - A website about jobs for IT</h1>
+    </>
+  )
+}
+
+export default App
